28b3a29c315ac759c19a083cd1099dc8f0d58756
YairTavor <YairTavor@gmail.com>
2014-04-21 18:45:36 +0000

    Added entities, routing now handle crud actions and custom actions.

--- Server/app.js
+++ Server/app.js
@@ -4,8 +4,11 @@
 	var server = require('./core/server.js'),
 		settings = require('./core/settings.js'),
 		db = require('./core/db.js'),
-		routing = require('./core/routing.js');
+		routing = require('./core/routing.js'),
+		activities = require('./entities/activity.js');
 
+	activities.register();
+	
 	db.open(settings, function dbOpend(){		
 		server.start(settings, routing);
 	});
--- Server/core/response-helper.js
+++ Server/core/response-helper.js
@@ -11,33 +11,46 @@
 			forbidden : 403,
 			notFound : 404,
 			badRequest : 400,
-			serverError : 500 
+			serverError : 500,
+			notImplemented : 501
 		},
 		headers = {
 			contentType : 'Content-Type'
 		};
 
-	function sendNotFound(response, message){
+	function sendSimpleMessage(response, message, code){
 		response.setHeader(headers.contentType, contentTypes.text);
-		response.statusCode = responseCodes.notFound;
+		response.statusCode = code;
 		response.end(message);
+	}
+
+	function sendNotFound(response, message){
+		sendSimpleMessage(response, message, responseCodes.notFound);
 	}
 
 	function sendOk(response, retrunValue){
 		response.setHeader(headers.contentType, contentTypes.json);
 		response.statusCode = responseCodes.ok;
-		response.end(retrunValue);
+		response.end(JSON.stringify(retrunValue));
 	}
 
 	function sendError(response, message){
-		response.setHeader(headers.contentType, contentTypes.text);
-		response.statusCode = responseCodes.serverError;
-		response.end(message);
+		sendSimpleMessage(response, message, responseCodes.serverError);
+	}
+
+	function sendBadRequest(response, message){
+		sendSimpleMessage(response, message, responseCodes.badRequest);
+	}
+
+	function sendNotImplemented(response, message){
+		sendSimpleMessage(response, message, responseCodes.notImplemented);
 	}
 
 	exports.send = {
-		notFound 	: sendNotFound,
-		ok			: sendOk,
-		error 		: sendError
+		notFound 		: sendNotFound,
+		ok				: sendOk,
+		error 			: sendError,
+		badRequest 		: sendBadRequest,
+		notImplemented 	: sendNotImplemented
 	};
 })();
